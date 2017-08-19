e561c665944d9bb6fcd86142a59e91d5286f7d48
fftehnik <fftehnik@yandex.ru>
2017-08-19 22:17:57 +0300

    download all files (without maxResults restriction) using nextPageToken; update Readme.md

--- Readme.md
+++ Readme.md
@@ -4,8 +4,8 @@ Currently (Aug 2017), "download all" button in web interface works buggy.
 For example some files can absent in result zip or zip create endless.  
 See more:
 1. https://productforums.google.com/forum/#!topic/drive/MTDDCtXTRyQ
-1. https://productforums.google.com/forum/#!msg/drive/m_uJ8TTQce0/sFEcUdL5AwAJ
-1. https://productforums.google.com/forum/#!msg/drive/YUseY-VPG-Q/8SCPCHQAAgAJ
+1. https://productforums.google.com/forum/#!topic/drive/m_uJ8TTQce0
+1. https://productforums.google.com/forum/#!topic/drive/YUseY-VPG-Q
 
 Another ways (via various applications) require authorization.
 
@@ -14,7 +14,6 @@ Script emulates browser behavior. It downloads list of files in folder (in JSON 
 To get list of files need to use some magic string - need to investigate, what is it and can this string change or not.
 
 # TODO
-*   check incompleteSearch
 *   add parallel download
 *   check successfull download
 *   add recursive downloads
--- gdrive_download_dir.py
+++ gdrive_download_dir.py
@@ -6,32 +6,46 @@ import requests
 
 GDRIVE_MAGIC_KEY = 'AIzaSyC1qbk75NzWBvSaDh6KnsjjA9pIrP4lYIE'
 GDRIVE_HOST = 'https://clients6.google.com'
-MAX_RESULTS = '5000'
+
+def get_filelist(folder_id):
+    files = dict()
+    next_page_token = ''
+    while True:
+        url = GDRIVE_HOST + '/drive/v2beta/files?' + \
+              '&q=\'' + folder_id + '\' in parents' + \
+              '&fields=items(mimeType,title,id),nextPageToken' + \
+              '&maxResults=50' + \
+              '&key=' + GDRIVE_MAGIC_KEY
+        if next_page_token != '':
+            url += '&pageToken=' + next_page_token
+        resp = requests.get(url, headers={'referer': sys.argv[1]})
+        res = json.loads(resp.content.decode('utf8').replace('\'', '"'))
+        for item in res['items']:
+            files[item['id']] = item['title']
+        if 'nextPageToken' in res.keys():
+            next_page_token = res['nextPageToken']
+        else:
+            break # got all filenames and id's
+    return files
+
+
+def download_file(file_id, filename):
+    print('Try to download ' + filename + ' from ' + file_id)
+    resp = requests.get('https://docs.google.com/uc?id=' + file_id)
+    file_jpeg = open(filename, 'wb+')
+    file_jpeg.write(resp.content)
+    file_jpeg.close()
 
 def main():
-    print("Try to download files from " + sys.argv[1])
+    print('Try to download files from ' + sys.argv[1])
     folder_id = sys.argv[1].rsplit('/', maxsplit=1)[1]
-    print("folder_id = " + folder_id)
-    flist = requests.get(GDRIVE_HOST + "/drive/v2beta/files?" +
-                         "&q='" + folder_id + "' in parents" +
-                         "&fields=items(mimeType,title,id),incompleteSearch" +
-                         "&maxResults=" + MAX_RESULTS +
-                         '&key=' + GDRIVE_MAGIC_KEY,
-                         headers={'referer': 'https://drive.google.com/drive/folders/' + folder_id})
-    print(flist.content.decode('utf8').replace("'", '"'))
-
-    res = json.loads(flist.content.decode('utf8').replace("'", '"'))
-
-    print("Start downloading " + str(len(res['items'])) + " files")
+    files = get_filelist(folder_id)
+    print('Start downloading ' + str(len(files)) + ' files')
     exit(0)
-    for item in res['items']:
-        print("Try to download " + item['title'] + ' from ' + item['id'])
 
-        resp = requests.get('https://docs.google.com/uc?id=' + item['id'])
-        file_jpeg = open(item['title'], 'wb+')
-        file_jpeg.write(resp.content)
-        file_jpeg.close()
-    print("Done")
+    for file_id, filename in files.items():
+        download_file(file_id, filename)
+    print('Done')
 
 
 if __name__ == '__main__':
