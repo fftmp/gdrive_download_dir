1957c7e15beb92568a4963afb73d3d450403d678
fftehnik <fftehnik@yandex.ru>
2017-08-19 17:44:08 +0300

    get file list from request, not manually

--- Readme.md
+++ Readme.md
@@ -1,8 +1,21 @@
-This script aims to allow download shared folders from google drive without authorization.
-Currently (Aug 2017), "download all" button in web interface works buggy. For example some files can absent in result zip or zip create endless.
+# About
+This script aims to allow download shared folders from google drive without authorization.  
+Currently (Aug 2017), "download all" button in web interface works buggy.  
+For example some files can absent in result zip or zip create endless.  
 See more:
 1. https://productforums.google.com/forum/#!topic/drive/MTDDCtXTRyQ
 1. https://productforums.google.com/forum/#!msg/drive/m_uJ8TTQce0/sFEcUdL5AwAJ
 1. https://productforums.google.com/forum/#!msg/drive/YUseY-VPG-Q/8SCPCHQAAgAJ
 
 Another ways (via various applications) require authorization.
+
+# How it works
+Script emulates browser behavior. It downloads list of files in folder (in JSON firmat), then download each file by file_id.
+To get list of files need to use some magic string - need to investigate, what is it and can this string change or not.
+
+# TODO
+*   check incompleteSearch
+*   add parallel download
+*   check successfull download
+*   add recursive downloads
+*   add option for download or skip trashed files
--- gdrive_download_dir.py
+++ gdrive_download_dir.py
@@ -4,16 +4,35 @@ import sys
 import json
 import requests
 
-print("Try to read file IDs and filenames from ", sys.argv[1])
+GDRIVE_MAGIC_KEY = 'AIzaSyC1qbk75NzWBvSaDh6KnsjjA9pIrP4lYIE'
+GDRIVE_HOST = 'https://clients6.google.com'
+MAX_RESULTS = '5000'
 
-res_file = open(sys.argv[1])
-res = json.load(res_file)
-res_file.close()
-print("Try to download " + str(len(res['items'])) + " files")
-for item in res['items']:
-    print("Try to download " + item['title'])
-    resp = requests.get('https://docs.google.com/uc?id=' + item['id'])
-    file_jpeg = open(item['title'], 'wb+')
-    file_jpeg.write(resp.content)
-    file_jpeg.close()
-print("Done")
+def main():
+    print("Try to download files from " + sys.argv[1])
+    folder_id = sys.argv[1].rsplit('/', maxsplit=1)[1]
+    print("folder_id = " + folder_id)
+    flist = requests.get(GDRIVE_HOST + "/drive/v2beta/files?" +
+                         "&q='" + folder_id + "' in parents" +
+                         "&fields=items(mimeType,title,id),incompleteSearch" +
+                         "&maxResults=" + MAX_RESULTS +
+                         '&key=' + GDRIVE_MAGIC_KEY,
+                         headers={'referer': 'https://drive.google.com/drive/folders/' + folder_id})
+    print(flist.content.decode('utf8').replace("'", '"'))
+
+    res = json.loads(flist.content.decode('utf8').replace("'", '"'))
+
+    print("Start downloading " + str(len(res['items'])) + " files")
+    exit(0)
+    for item in res['items']:
+        print("Try to download " + item['title'] + ' from ' + item['id'])
+
+        resp = requests.get('https://docs.google.com/uc?id=' + item['id'])
+        file_jpeg = open(item['title'], 'wb+')
+        file_jpeg.write(resp.content)
+        file_jpeg.close()
+    print("Done")
+
+
+if __name__ == '__main__':
+    main()
