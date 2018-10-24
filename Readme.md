7ebff395e782f237f16b7de1f74e9e9ce0502235
fftehnik <fftehnik@yandex.ru>
2018-10-24 22:55:36 +0300

    Fix: create empty dirs. Fix: download big files. Use different approach. Measure time for get items list and for downloading. Add note about rename files in Readme.md

--- Readme.md
+++ Readme.md
@@ -2,6 +2,7 @@
 This script aims to allow download shared folders from google drive without authorization.  
 Currently (Aug 2017), "download all" button in web interface works buggy.  
 For example some files can absent in result zip or zip create endless.  
+Filenames with special symbols (at least newline and dot at beginning) are mangled (special symbol changes to underscore).
 See more:
 1. https://productforums.google.com/forum/#!topic/drive/MTDDCtXTRyQ
 1. https://productforums.google.com/forum/#!topic/drive/m_uJ8TTQce0
@@ -12,8 +13,7 @@ Another ways (via various applications) require authorization.
 # How it works
 Script emulates browser behavior. It downloads list of files in folder (in JSON format), then download each file by file_id.
 To get list of files need to use some magic string - need to investigate, what is it and can this string change or not.
-For downloading big files (those, that can't verified against viruses) was tried idea from [user115202 on stackoverflow](https://stackoverflow.com/a/39225039),
-but it is not works for now. Workable solution is to POST (important) ordinary url with file_id and get true download url from response.
+For downloading big files (those, that can't verified against viruses) used idea from [turdus-merula and Andrew Hundt on stackoverflow](https://stackoverflow.com/a/39225039),
 
 
 # Usage example
@@ -23,3 +23,4 @@ Currently script creates folder with given folder_id in cwd and place there down
 
 # TODO
 *   increase downloading speed, if possible
+*   make tests
--- gdrive_download_dir.py
+++ gdrive_download_dir.py
@@ -10,9 +10,9 @@ import sys
 import json
 import logging as log
 from os import makedirs
-from os.path import getsize, exists
+from os.path import exists
 from multiprocessing.dummy import Pool as ThreadPool
-from time import sleep
+from time import sleep, time
 import shutil
 import requests
 
@@ -23,12 +23,12 @@ DOWNLOAD_ATTEMPTS = 5
 TIMEOUT = 10
 
 def get_dir_tree(dir_name, dir_id, ses=None):
-    """Return info about files and dirs inside list of dict: [{size : '', name: '', _id: ''}]
+    """Return info about files and dirs inside list of dict: [{size : '', name: '', id: ''}]
        for given dir_id. Names are relative to dir. Call itself recursive for subdirs.
     """
     log.debug('get file list %s', dir_name)
     if not ses:
-        ses = requests.Session() #create session for activate keep alive
+        ses = requests.Session()
     tree = list()
     next_page_token = ''
     url = GDRIVE_HOST + '/drive/v2beta/files?'
@@ -49,86 +49,82 @@ def get_dir_tree(dir_name, dir_id, ses=None):
         for item in res['items']:
             if item['mimeType'] == 'application/vnd.google-apps.folder':
                 nested_tree = get_dir_tree(item['title'], item['id'], ses)
+                if not nested_tree:
+                    #empty folder
+                    tree.append({'id' : item['id'],
+                                 'name' : dir_name + '/' + item['title'] + '/',
+                                 'size' : 0})
                 for _it in nested_tree:
                     _it['name'] = dir_name + '/' + _it['name']
                 tree.extend(nested_tree)
             else:
-                tree.append({'_id' : item['id'], 'name' : dir_name + '/' + item['title'],
+                tree.append({'id' : item['id'],
+                             'name' : dir_name + '/' + item['title'],
                              'size' : int(item['fileSize'])})
-            tree.append({'_id': dir_id, 'name': dir_name + '/'})
         if 'nextPageToken' in res.keys():
             next_page_token = res['nextPageToken']
         else: # got all filenames and id's
             return tree
 
 
-def download_file(item, skip_exist=True):
+def download_file(id_, name, overwrite=False):
     """ Download file and save it to name. Create dirs, if necessary.
         If name end with '/', treat it as a dir name an only create dir.
     """
-    if item['name'].endswith('/'):
+    if name.endswith('/'):
         #this is dir, not a file
-        makedirs(item['name'], exist_ok=True)
+        makedirs(name, exist_ok=True)
         return
 
-    if skip_exist and exists(item['name']) and item['size'] == getsize(item['name']):
-        log.debug('Skip downloading already existed file with correct size: ' +
-                  item['name'] + ' id = ' + item['_id'])
+    if exists(name) and not overwrite:
+        log.debug('%s already exists. Skipping.', name)
         return
 
-    path = item['name'].rsplit('/', maxsplit=1)[0]
-    log.debug('Download ' + item['name'] + ' from ' + item['_id'])
+    path = name.rsplit('/', maxsplit=1)[0]
+    log.debug('Download %s from %s', name, id_)
     makedirs(path, exist_ok=True)
 
     for i in range(DOWNLOAD_ATTEMPTS):
         try:
-            resp = requests.get('https://drive.google.com/uc?', params={'id': item['_id']},
-                                stream=True, timeout=TIMEOUT)
+            session = requests.Session()
+            resp = session.get('https://drive.google.com/uc?', params={'id': id_},
+                               stream=True, timeout=TIMEOUT)
             resp.raise_for_status()
-            for cookie_name in resp.cookies.keys():
+            for cookie_name, cookie_value in resp.cookies.items():
                 if cookie_name.startswith('download_warning'):
-                    log.debug('big file ' + item['name'] + ' id = ' + item['_id'])
-                    resp = requests.post('https://drive.google.com/uc?', params={'id': item['_id']})
+                    log.debug('big file %s with id = %s', name, id_)
+                    resp = session.get('https://drive.google.com/uc?',
+                                       params={'id': id_, 'confirm': cookie_value}, stream=True)
                     resp.raise_for_status()
-                    # in response first line contain garbage. so we cut it
-                    resp = requests.get(json.loads(resp.content.decode('utf8').
-                                                   split('\n', maxsplit=1)[1])
-                                        ['downloadUrl'],
-                                        stream=True, timeout=TIMEOUT)
                     break
-            with open(item['name'], 'wb') as _f:
+            with open(name, 'wb') as _f:
                 resp.raw.decode_content = True
                 shutil.copyfileobj(resp.raw, _f)
-            break
+                return
         except (requests.exceptions.RequestException, requests.exceptions.BaseHTTPError) as _e:
             if i < DOWNLOAD_ATTEMPTS - 1:
                 log.warning('%s during download file %s with id = %s. Retrying.', type(_e).__name__,
-                            item['name'], item['_id'])
+                            name, id_)
                 sleep(2)
             else:
-                log.error('Couldn\'t download file %s with id = %s. Skipping.', item['name'],
-                          item['_id'])
+                log.error('Couldn\'t download file %s with id = %s. Skipping.', name, id_)
                 return
-
-    real_size = getsize(item['name'])
-    if  real_size != item['size']:
-        raise Exception('Error during download file ' + item['name'] +
-                        '. Expected size = ' + str(item['size']) + '. Get size = ' + str(real_size))
 
 def download_dir_recursive(src_dir_id, dst_dir):
     """ Download all files and dirs (including empty dirs) from dir with id=src_dir_id
         and save result in dst_dir.
     """
     log.info('Start downloading %s to %s', src_dir_id, dst_dir)
+    begin_ts = time()
     tree = get_dir_tree(src_dir_id, dst_dir)
-    total_size = 0
-    for _it in tree:
-        if 'size' in _it.keys():
-            total_size += _it['size']
-    log.info('Start downloading files. Total size = %d bytes', total_size)
+    log.debug('get_dir_tree executed in %d seconds', time() - begin_ts)
+    total_size = sum(_it['size'] for _it in tree)
 
+    log.info('Start downloading files. Total size = %d bytes', total_size)
     pool = ThreadPool(DOWNLOAD_THREADS)
-    pool.map(download_file, tree)
+    begin_ts = time()
+    pool.starmap(download_file, [(elem['id'], elem['name']) for elem in tree])
+    log.debug('parallel download_file executed in %d seconds', time() - begin_ts)
     pool.close()
     pool.join()
 
